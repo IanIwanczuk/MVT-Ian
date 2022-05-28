10148f8488c125361e6d253c666b583004d80077
Ian Iwanczuk <a345587553455875@gmail.com>
2022-05-29 01:33:04 +0200

    Adding family

--- FamiliaUno/urls.py
+++ FamiliaUno/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import path
 
 from FamiliaUno.views import prueba
+from familiares.views import familiares
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('prueba/', prueba, name = "prueba"),
+    path('familiares/', familiares, name = "familiares"),
 ]
--- familiares/views.py
+++ familiares/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+from familiares.models import Familiares
+
 # Create your views here.
+def familiares(request):
+    famliar_nuevo = Familiares.objects.create(
+            nombre = "Ian",
+            apellido = "Iwanczuk",
+            nacimiento = "02/10/2022",
+            edad = "19",
+            )
+    context = {'familiar_nuevo': famliar_nuevo}
+    return render(request, 'familiares.html', context=context)
